dfadc4084f816d7319981a6eb910b86ede700a50
Alexios Collins <93901055+AlexiosEC@users.noreply.github.com>
2023-03-02 17:02:27 -0600

    Update urls.py
    Added paths to home and login page

--- LibMS/libmsapp/urls.py
+++ LibMS/libmsapp/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('search/', views.search_books, name='search_books'),
+    path('login/', views.login, name='login'),
 ]
