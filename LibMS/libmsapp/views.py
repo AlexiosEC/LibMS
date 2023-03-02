38d7bd931d8e0da165c0a04d55bfd26a0d6468cb
Alexios Collins <93901055+AlexiosEC@users.noreply.github.com>
2023-03-02 16:38:41 -0600

    Added search_books function

--- LibMS/libmsapp/views.py
+++ LibMS/libmsapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Book
 
 # Create your views here.
+def search_books(request):
+    return render(request, 'search.html')
