232496c69fa352c924ff72559dd67aa026c88e7c
Alexios Collins <93901055+AlexiosEC@users.noreply.github.com>
2023-03-02 16:43:26 -0600

    Update views.py
    Updated search_books function to handle search requests.

--- LibMS/libmsapp/views.py
+++ LibMS/libmsapp/views.py
@@ -4,4 +4,13 @@ from .models import Book
 
 # Create your views here.
 def search_books(request):
-    return render(request, 'search.html')
+    query = request.GET.get('query')
+    if query:
+        results = Book.objects.filter(
+            Q(title__icontains=query) |
+            Q(author__icontains=query) |
+            Q(description__icontains=query)
+        )
+    else:
+        results = []
+    return render(request, 'search.html', {'results': results})
