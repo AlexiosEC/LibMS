7263cf390aba21bd755a96c9e845d75ea933a20a
Alexios Collins <93901055+AlexiosEC@users.noreply.github.com>
2023-04-23 13:40:50 -0500

    Update urls.py
    Taking out "Home" page.

--- LibMS/libmsapp/urls.py
+++ LibMS/libmsapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('search/', views.search_books, name='search_books'),
     path('login/', views.login, name='login'),
 ]
