99263635c32b70e3390fef30dfc1ae6cfa19d640
Alexios Collins <93901055+AlexiosEC@users.noreply.github.com>
2023-04-23 17:07:17 -0500

    Update models.py

--- LibMS/libmsapp/models.py
+++ LibMS/libmsapp/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    description = models.TextField()
-    published_date = models.DateField()
-    isbn = models.CharField(max_length=13, blank=True)
-    available = models.BooleanField(default=True, null=False)
-
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
